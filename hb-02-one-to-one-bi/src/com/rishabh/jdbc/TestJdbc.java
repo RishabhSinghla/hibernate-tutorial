7a9d4f2f088d1cf5c936e5414f0214baedc61045
Rishabh Singhla <42176920+RishabhSinghla@users.noreply.github.com>
2022-10-04 14:44:01 +0530

    Add files via upload

---
+++ hb-02-one-to-one-bi/src/com/rishabh/jdbc/TestJdbc.java
@@ -0,0 +1,30 @@
+package com.rishabh.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String jdbcurl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			
+			System.out.println("Connecting to database: " + jdbcurl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcurl, user, pass);
+			
+			System.out.println("Connection succesful");
+			
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
